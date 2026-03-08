aadef3325fe33da8259a6f34d758262457de428d
pivoshenko <pivoshenko@users.noreply.github.com>
2026-03-08 09:09:56 +0000

    build(dev): upgrade next.js to 16.1.6

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next'
 
 const config: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
 
 export default config
